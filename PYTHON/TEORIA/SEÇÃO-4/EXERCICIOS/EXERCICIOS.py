b6cf76b79c8610d3fa6705ae8ca337e1732c1035
AngelinaPierre <231Ac170>
2021-05-02 16:37:36 -0300

    q40 exercise unfinished

--- PYTHON/TEORIA/SEÇÃO-4/EXERCICIOS/EXERCICIOS.py
+++ PYTHON/TEORIA/SEÇÃO-4/EXERCICIOS/EXERCICIOS.py
@@ -79,42 +79,185 @@
 # print(f'Your distance in kilometers {km} is in miles = {miles}')
 
 # Q14
+# print('Type angle in degress:')
+# deg = float(input())
+# pi = 3.14
+# rad = deg * (pi/180)
+# print(f'The angle {deg} in radians is {rad}')
 
+# Q15
+# print('Type angle in radians:')
+# rad = float(input())
+# pi = 3.14
+# deg = rad * (180/pi)
+# print(f'The angle {rad} in degrees is {deg}')
 
+# Q16
+# print('Type lenght in inches:')
+# inches = float(input())
+# cent = inches * 2.54
+# print(f'Value in inches {inches} equals centimeters {cent}')
 
+# Q17
+# print('Type lenght in centimeters:')
+# cent = float(input())
+# inches = cent/2.54
+# print(f'Value in centimeters {cent} equals inches {inches}')
 
+# Q18
+# print('Type volume in cubic meters:')
+# cm = float(input())
+# liters = 1000 * cm
+# print(f'Volume in liter equals {liters}')
 
+# Q19
+# print('Type volume in liters:')
+# liters = float(input())
+# cm = liters/1000
+# print(f'Volume in Cubic meters equals {cm}')
 
+# Q20
+# print('Type physical mass in KG:')
+# kg = float(input())
+# pounds = kg/0.45
+# print(f'Physical Mass in pounds equals {pounds}')
 
+# Q21
+# print('Type physical mass in pounds:')
+# pounds = float(input())
+# kg = pounds * 0.45
+# print(f'Physical Mass in kg equals {kg}')
 
+# Q22
+# print('Type lenght in yards:')
+# yards = float(input())
+# meters = yards * 0.91
+# print(f'yards = {yards} | meters = {meters}')
 
+# Q23
+# print('Type lenght in meters:')
+# meters = float(input())
+# yards = meters / 0.91
+# print(f'lenght meters = {meters} | yards = {yards}')
 
+# Q24
+# print('Type value in square meters:')
+# Smeters = float(input())
+# acres = Smeters * 0.000247
+# print(f' Value meters = {Smeters} | acres = {acres} ')
 
+# Q25
+# print('Type value in acres:')
+# acres = float(input())
+# meters = acres * 4048.58
+# print(f'value acres = {acres} | square meters = {meters}')
 
+# Q26
+# print('Type value in square meters:')
+# meters = float(input())
+# hect = meters * 0.0001
+# print(f'Vlue meters = {meters} | acre = {hect}')
 
+# Q27
+# print('Type value in Acre:')
+# acre = float(input())
+# smeters = acre * 10000
+# print(f'Value in acre = {acre} | Square Meters = {smeters}')
 
+# Q28
+# print(' Type 3 values:')
+# n1 = float(input())
+# n2 = float(input())
+# n3 = float(input())
+#
+# square_add = (n1**2) + (n2**2) + (n3**2)
+#
+# print(f'{n1}^2 + {n2}^2 + {n3}^2 = {square_add} ')
 
+# Q29
+# print('Type 4 grades:')
+# n1 = float(input())
+# n2 = float(input())
+# n3 = float(input())
+# n4 = float(input())
+#
+# med_ari = (n1 + n2 + n3 + n4)/4
+#
+# print(f'Arithmetic Average = {med_ari}')
 
+# Q30
+# print('Type one value and dolar cotation:')
+# value = float(input())
+# dolar = float(input())
+# vdolar = value * dolar
+# print(f'value = {value} | value in dorlar = {vdolar}')
 
+# Q31
+# print('type a integer value:')
+# inter = int(input())
+# ant = inter - 1
+# pos = inter + 1
+# print(f'value = {inter} | predecessor = {ant} | sucessor = {pos}')
 
+# Q32
+# print('Type an integer value:')
+# inter = int(input())
+# trip = (inter ** 3) + 1
+# double = (inter ** 2) - 1
+# add = trip + double
+# print(f' value = {inter} |trip = {trip} | double = {double} | add = {add}')
 
+# Q33
+# print('Type the one side of the square:')
+# side = float(input())
+# area = side **2
+# print(f'size = {side} | area = {area}')
 
+# Q34
+# print('Type circle ray:')
+# ray = float(input())
+# pi = 3.141592
+# area = (ray ** 2) * pi
+# print(f'ray = {ray} | area = {area}')
 
+# Q35
+# print('Type the two sides of a triangle:')
+# a = float(input())
+# b = float(input())
+# # math.pow(num, 1/2)
+# hip = pow((a**2) + (b**2), 1/2)
+# print(f' a = {a} | b = {b} | hip = {hip}')
 
+# Q36
+# print('Type the height and ray of the cylinder:')
+# height = float(input())
+# ray = float(input())
+# pi = 3.141592
+# volume = pi * (ray**2) * height
+# print(f'Height = {height} | ray = {ray} | Volume = {volume}')
 
+# Q37
+# print('Type the value of the product:')
+# value = float(input())
+# discount = value * (12/100)
+# print(f'Value Product = {value} | Value Product Discount(12%) = {value - discount}')
 
+# Q38
+# print('Type the wage:')
+# wage = float(input())
+# new_wage = wage + (wage * (25/100))
+# print(f'Old wage = {wage} | new Wage = {new_wage}')
 
+# Q39
+# qtd = 780_000
+# first = qtd * 0.46
+# qtd = qtd - first
+# second = qtd * 0.32
+# third = qtd - second
+# # third = qtd * 0.78
+# print(f'1 - {first} | 2 - {second} | 3 - {third}')
 
-
-
-
-
-
-
-
-
-
-
+# Q40
 
 
 
