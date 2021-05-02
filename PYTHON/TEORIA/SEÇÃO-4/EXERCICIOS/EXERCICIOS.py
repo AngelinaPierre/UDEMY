77af844651ec295ce1edc7402dc50332e283d255
AngelinaPierre <gabrielle.tripodi@hotmail.com>
2021-05-02 11:04:00 -0300

    cont python exercicios s4

--- PYTHON/TEORIA/SEÇÃO-4/EXERCICIOS/EXERCICIOS.py
+++ PYTHON/TEORIA/SEÇÃO-4/EXERCICIOS/EXERCICIOS.py
@@ -8,7 +8,22 @@
 # print(num)
 # print(type(num))
 
-#Q3
+# Q3
+# print('type 3 integer values:')
+# n1 = int(input())
+# n2 = int(input())
+# n3 = int(input())
+# num = n1 + n2 + n3
+# print(num)
+# print(type(n1))
+# print(type(n2))
+# print(type(n3))
+# print(type(num))
 
+# Q4
+# num = 1.44
+# el = num ** 2
+# print(el)
 
+# Q5
 
