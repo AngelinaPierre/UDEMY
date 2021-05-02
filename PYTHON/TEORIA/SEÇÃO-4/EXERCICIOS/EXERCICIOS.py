0871bdcd22f09938dde746f6bb739c5a24f5df15
AngelinaPierre <gabrielle.tripodi@hotmail.com>
2021-05-02 11:36:20 -0300

    cont python exercicios s4

--- PYTHON/TEORIA/SEÇÃO-4/EXERCICIOS/EXERCICIOS.py
+++ PYTHON/TEORIA/SEÇÃO-4/EXERCICIOS/EXERCICIOS.py
@@ -3,7 +3,7 @@
 # print(num)
 # print(type(num))
 
-#Q2
+# Q2
 # num = 1.3
 # print(num)
 # print(type(num))
@@ -26,4 +26,100 @@
 # print(el)
 
 # Q5
+# num = 1.45678
+# quint = int(num / (1/5))
+# print(f' A quinta parte de {num} eh = {quint}')
+
+# Q6
+# print('type the temperature on Celcius:')
+# celcius = float(input())
+# fah = celcius * (9/5) + 32
+# print(f'Temperature in Fahrenheit = {fah}')
+
+# Q7
+# print('type the temperature on Fahrenheit:')
+# fah = float(input())
+# celcius = 5 * (fah - 32)/9
+# print(f'Temperature in Fahrenheit = {celcius}')
+
+# Q8
+# print('Type the temperature on Kelvin:')
+# kel = float(input())
+# cel = kel - 273.15
+# print(f'Temperature in Celcius = {cel}')
+
+# Q9
+# print('type the temperature on Celcius:')
+# cel = float(input())
+# kel = cel + 273.15
+# print(f'Temperature in kelvin = {kel}')
+
+# Q10
+# print('Type the velocity in Km/h:')
+# km = float(input())
+# ms = km/(3.6)
+# print(f'the velocity {km} km/h = {ms} m/s')
+
+# Q11
+# print('Type the velocity in m/s:')
+# ms = float(input())
+# km = ms * (3.6)
+# print(f'The velocity {ms} m/s = {km} km/h')
+
+# Q12
+# print('Type your distance in miles:')
+# miles = float(input())
+# km = 1.61 * miles
+# print(f'Your miles distance {miles} is in kilometers = {km}')
+
+# Q13
+# print('Type your distance in kilometers:')
+# km = float(input())
+# miles = km/(1.61)
+# print(f'Your distance in kilometers {km} is in miles = {miles}')
+
+# Q14
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
