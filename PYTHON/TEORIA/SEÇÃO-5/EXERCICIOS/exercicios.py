5eff37c9a44246e43dca4a3b17f33f0201bb2f7d
AngelinaPierre <231Ac170>
2021-05-04 09:29:45 -0300

    q40 exercise unfinished

---
+++ PYTHON/TEORIA/SEÇÃO-5/EXERCICIOS/exercicios.py
@@ -0,0 +1,13 @@
+# Q1
+# print('Type to numbers:')
+# a = float(input())
+# b = float(input())
+#
+# if a > b:
+#     print(f'Numero a = {a} eh maior que numero b {b}')
+# else:
+#     print(f'Numero b = {b} é maior que numero a = {a}')
+
+# Q2
+print()
+
