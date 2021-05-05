57717273702f7ec17c5b6a350ebe220078e41785
AngelinaPierre <231Ac170>
2021-05-05 01:25:17 -0300

    Q9 season 5 unfinished

--- PYTHON/TEORIA/SEÇÃO-5/EXERCICIOS/exercicios.py
+++ PYTHON/TEORIA/SEÇÃO-5/EXERCICIOS/exercicios.py
@@ -9,5 +9,103 @@
 #     print(f'Numero b = {b} é maior que numero a = {a}')
 
 # Q2
-print()
+# print('Type a number:')
+# number = float(input())
+#
+# if number > 0:
+#     sqr = pow(number,1/2)
+#     print(f'sqr of number {number} = {sqr}')
+# else:
+#     print('Invalid number')
+
+# Q3
+
+# print('Type a real number:')
+# r_nbm = float(input())
+#
+# if r_nbm > 0:
+#     sqr = pow(r_nbm,1/2)
+#     print(f'SQR of real number {r_nbm} = {sqr}')
+# else:
+#     number_two = r_nbm ** 2
+#     print(f'Real number {r_nbm} ^2 = {number_two}')
+
+# Q4
+
+# print('Type a number:')
+# number = float(input())
+#
+# if number > 0:
+#     number_t = number ** 2
+#     sqr_number = pow(number,1/2)
+#     print(f'Number {number} ^2 = {number_t} | nSQR = {sqr_number}')
+
+# Q5
+
+# print('Type a number')
+# number = int(input())
+#
+# if number % 2 == 0:
+#     print(f'Numero {number} par')
+# else:
+#     print(f'Numero {number} impar')
+
+# Q6
+
+# print(' Type two numbers:')
+# n1 = int(input())
+# n2 = int(input())
+#
+# if n1 > n2:
+#     dif = n1 - n2
+#     print(f'n1 {n1} is bigger | Diference of = {dif}')
+# else:
+#     dif = n2-n1
+#     print(f'N2 {n2} is bigger | diference = {dif}')
+
+# Q7
+
+# print('Type two numbers:')
+# n1 = float(input())
+# n2 = float(input())
+#
+# if n1 > n2:
+#     print(f'N1 {n1} is bigger')
+# elif n2 > n1:
+#     print(f'N2 {n2} is bigger')
+# else:
+#     print('Equal numbers')
+
+# Q8
+
+# print('Type two grades:')
+# n1 = float(input())
+# n2 = float(input())
+
+# if n1 < 0.0:
+#     print('Invalid Grade')
+# elif n1 > 10.0:
+#     print('Invalid Grade')
+# elif n2 < 0.0:
+#     print('Invalid Grade')
+# elif n2 > 10.0:
+#     print('iNvalid grade')
+# else:
+#     med = (n1 + n2)/2
+#     print(f'Grade = {med}')
+
+# another way
+
+# if n1 < 0.0 or n1 > 10.0:
+#     print('Invalid Grade')
+# elif n2 < 0.0 or n2 > 10.0:
+#     print('Invalid Grade')
+# else:
+#     med = (n1 + n2)/2
+#     print(f'Grade = {med}')
+
+# Q9
+
+
+
 
